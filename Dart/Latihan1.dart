04173c3456d67318f3685bd32cc41c75f073d227
Muhammad Ramzi Marasabessy <ramzimarssy@gmail.com>
2025-08-26 12:00:41 +0900

    Add files via upload

---
+++ Dart/Latihan1.dart
@@ -0,0 +1,15 @@
+
+
+void main() {
+  String nama = ("Ramzi");
+  int npm = 07352211131;
+  num Tinggi = 163;
+  dynamic Usia = 20;
+  bool isActive = false;
+
+  print("Nama saya adalah: $nama");
+  print("NPM saya adalah: $npm");
+  print("Tinggi saya adalah: $Tinggi cm");
+  print("Berat saya adalah: $Usia ");
+  print("Saya masih aktif kuliah: $isActive");
+}
